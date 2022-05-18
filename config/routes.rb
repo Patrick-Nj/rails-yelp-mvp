8ef5ad8417c140b0236ceb10d8ecf5d5e304397a
patrick njoume <p.njoume@gmail.com>
2022-05-19 01:21:15 +0200

    ex2

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "restaurants#index"
+  resources :restaurants
+  # get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post '/restaurants', to: 'restaurants#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating restaurants...'
+
+nicoletta = { name: 'Nicoletta', address: '21 rue bonnenfant, 78100', category: 'italian', phone_number: '0182830083' }
+wagon_bleu = { name: 'Wagon bleu', address: '7 rue boursault,75008', category: 'french', phone_number: '0145223525' }
+fuxia = { name: 'Fuxia', address: '40 rue de la republique, 78100', category: 'italian', phone_number: '0148789325' }
+shika = { name: 'Shika', address: '19 Rue Jean Mermoz, 75008 Paris', category: 'japanese', phone_number: '0143592924' }
+cheesers = { name: 'Cheesers', address: '3 Rue Yves Toudic, 75010 Paris', category: 'french', phone_number: '0182830083' }
+
+[nicoletta, wagon_bleu, fuxia, shika, cheesers].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
